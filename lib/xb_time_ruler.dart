c6e7cf71b1248d90f53d9bc049ce8538b75f7d59
谢贤彬 <xiexianbin@jftech.com>
2024-01-25 13:52:42 +0800

    base commit

---
+++ lib/xb_time_ruler.dart
@@ -0,0 +1,7 @@
+library xb_time_ruler;
+
+export './src/xb_time_ruler_area.dart';
+export './src/xb_time_ruler_cropper.dart';
+export './src/xb_time_ruler_playback.dart';
+export './src/xb_time_ruler_segment.dart';
+export 'src/xb_time_ruler_raw.dart';
